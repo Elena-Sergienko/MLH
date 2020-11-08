8c7962b9f5313fd5fc8e485c6516b311f8fb24d3
Elena-Sergienko <elena.serg.us@gmail.com>
2020-11-08 12:56:15 -0800

    Added test suite for element Story (Jira MLH-499)

--- test/smoke/mainFunctionality.js
+++ test/smoke/mainFunctionality.js
@@ -3,6 +3,7 @@ const exp = require('../../data/expected.json');
 const data = require('../../data/testData.json');
 const inputValues4 = require('../../helpers/inputValues4');
 const inputValues4andClick = require('../../helpers/inputValues4andClick');
+const uploadingImage = require('../../helpers/uploadingImage');
 
 
 describe('Checking the main functionality', function () {
@@ -10,21 +11,34 @@ describe('Checking the main functionality', function () {
         browser.url('https://qa-apps.netlify.app/app_my_hero');
     });
 
-    describe('Happy path', function () {
-
+    describe('Happy path without image', function () {
         it('TC-7.007 Verify that User can read the story after submitting with choice type of story "Comedy"', function () {
-            inputValues4(data.names.LadyBug007, data.gender.she, data.ages.n5, data.typeOfStory.comedy);
-            $(sel.createButton).click();
-            const tryAgain = $(sel.tryAgain).isDisplayed();
-            expect(tryAgain).toEqual(true);
+            inputValues4andClick(data.names.LadyBug007, data.gender.she, data.ages.n5, data.typeOfStory.comedy);
+            const text = $(sel.story.textOfStory).isDisplayed();
+            expect(text).toEqual(true);
+
+            $(sel.tryAgain).click();
+            const homePage = $(sel.description).isDisplayed();
+            expect(homePage).toEqual(true);
         });
     });
 
-    describe('Other paths', function () {
-        it('TC xxx ', function () {
-            inputValues4andClick(data.names.LadyBug, data.gender.he, data.ages.n123, data.typeOfStory.overcomingTheMonster);
-            const tryAgain = $(sel.tryAgain).isDisplayed();
-            expect(tryAgain).toEqual(true);
+    describe('Happy path with image', function () {
+        it('TC Verify that User can read the story after submitting with choice type of story "Comedy" and with image', function () {
+            inputValues4(data.names.Shrek, data.gender.he, data.ages.n230, data.typeOfStory.comedy);
+            uploadingImage(data.images.shrekPng);
+            $(sel.createButton).click();
+
+            const imageCheck = $(sel.story.image).getAttribute('src');
+            const imageInStoryPresent = imageCheck.length > exp.story.srcIfNoImageInStory.length;
+            expect(imageInStoryPresent).toEqual(true);
+
+            const text = $(sel.story.textOfStory).isDisplayed();
+            expect(text).toEqual(true);
+
+            $(sel.tryAgain).click();
+            const homePage = $(sel.description).isDisplayed();
+            expect(homePage).toEqual(true);
         });
     });
 });
